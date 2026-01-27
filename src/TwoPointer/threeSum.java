bb37f4ffd2279985a453497bc15965e65ef86ebe
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-01-28 00:11:15 +0530

    qns

--- src/TwoPointer/threeSum.java
+++ src/TwoPointer/threeSum.java
@@ -1,4 +1,35 @@
 package TwoPointer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class threeSum {
-}
+   //https://leetcode.com/problems/3sum/
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Arrays.sort(nums);
+            for(int i =0;i<nums.length-2;i++){
+                if(i>0&&nums[i]==nums[i-1]) continue;
+                int left = i+1;
+                int right=nums.length-1;
+                while(left<right){
+                    int sum = nums[i]+nums[left]+nums[right];
+                    if(sum==0){
+                        ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < 0) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+
+
