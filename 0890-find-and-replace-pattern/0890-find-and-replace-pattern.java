aa9592c79453351efb6fd1079a220d010afca7ce
Utkarsh Gupta <utkarshgupta2803@gmail.com>
2026-06-27 23:16:53 +0530

    Time: 1 ms (78.01%), Space: 43.9 MB (54.56%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,11 +1,9 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         boolean[] arr = new boolean[words.length];
+         List<String> list = new ArrayList<>();
         for(int i =0;i<words.length;i++){
             arr[i] = isIsomorphic(words[i],pattern);
-        }
-        List<String> list = new ArrayList<>();
-        for(int i =0;i<words.length;i++){
             if(arr[i]) list.add(words[i]);
         }
         return list;
