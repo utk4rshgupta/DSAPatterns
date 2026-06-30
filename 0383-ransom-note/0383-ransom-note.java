49a7a4583b308c196eb0719c105db75e832cb069
Utkarsh Gupta <utkarshgupta2803@gmail.com>
2026-06-30 22:29:01 +0530

    Time: 1 ms (99.96%), Space: 46.3 MB (67.82%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
