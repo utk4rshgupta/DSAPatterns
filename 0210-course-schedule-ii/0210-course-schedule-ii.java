676c76a989ab8639e492a36846c4489fa68ee2c4
Utkarsh Gupta <utkarshgupta2803@gmail.com>
2026-08-11 08:36:01 +0530

    Time: 4 ms (93.91%), Space: 47 MB (38.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,46 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
+        List<List<Integer>> adj = new ArrayList<>();
         int[] vis = new int[numCourses];
-        int[] pathVis = new int[numCourses];
+        int[] pathVis  = new int[numCourses];
+        Stack<Integer> st = new Stack<>();
 
-        for(int i =0;i<numCourses ;i++){
+        for(int i =0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int edge[] : prerequisites){
+
+        for(int[] edge : prerequisites){
             adj.get(edge[1]).add(edge[0]);
         }
 
-        Stack<Integer> st = new Stack<>();
-        for(int i =0;i<numCourses ;i++){
-            if(vis[i] == 0){
+        for(int i =0;i<numCourses;i++){
+            if(vis[i]==0){
                 if(dfs(i , adj , vis , pathVis , st)) return new int[]{};
             }
         }
-        int[] ans = new int[numCourses];
+
+        int[]  ans = new int[st.size()];
+
         int i =0;
         while(! st.isEmpty()){
-            ans[i++] = st.peek();
-            st.pop();
+            ans[i++]  = st.pop();
         }
         return ans;
-        
     }
 
-    private boolean dfs(int node , ArrayList<ArrayList<Integer>> adj , int[] vis , int[] pathVis , Stack<Integer> st){
+    private boolean dfs(int node ,List<List<Integer>> adj,int[] vis , int[] pathVis , Stack<Integer> st ){
         vis[node] = 1;
-        pathVis[node] = 1 ;
+        pathVis[node] =  1;
 
-        for(int it : adj.get(node)){
+        for(int it: adj.get(node)){
             if(vis[it] == 0){
-                if(dfs(it , adj , vis , pathVis, st)) return true;;
-            }else if(pathVis[it]  == 1) return true;
+                if(dfs(it , adj , vis , pathVis , st)) return true;
+            }else if(pathVis[it] == 1){
+                return true;
+            }
         }
-        st.push(node);
         pathVis[node] = 0;
+        st.push(node);
         return false;
     }
 }
