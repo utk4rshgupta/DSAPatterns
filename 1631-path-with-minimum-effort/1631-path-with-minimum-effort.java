649bc59e0f9782f2c8f3cdd96815140cca881dc7
Utkarsh Gupta <utkarshgupta2803@gmail.com>
2026-08-20 10:08:52 +0530

    Time: 45 ms (44.89%), Space: 47.9 MB (59.4%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,13 +1,16 @@
 class Solution {
     int n, m;
+    int[][] heights;
+    PriorityQueue<int[]> pq;
     public int minimumEffortPath(int[][] heights) {
+        this.heights = heights;
         n = heights.length;
         m = heights[0].length;
         int[][] dist = new int[n][m];
         for(int[] row : dist){
             Arrays.fill(row , (int)1e9);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
+        pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
         dist[0][0] =0;
         pq.offer(new int[]{0,0,0});
 
@@ -19,15 +22,15 @@ class Solution {
 
             if(row == n-1 && col == m-1) return effort;
 
-            move(effort , dist , row , col ,  row+1 , col ,heights,pq);
-            move(effort , dist , row , col ,  row-1 , col ,heights,pq);
-            move(effort , dist , row , col ,  row  , col+1 ,heights,pq);
-            move(effort , dist , row , col ,  row  , col-1 ,heights,pq);
+            move(effort , dist , row , col ,  row+1 , col );
+            move(effort , dist , row , col ,  row-1 , col );
+            move(effort , dist , row , col ,  row  , col+1);
+            move(effort , dist , row , col ,  row  , col-1);
         }
         return 0;
     }
 
-    private void move(int effort , int[][] dist ,int row , int col, int newRow , int newCol , int[][] heights , PriorityQueue<int[]> pq){
+    private void move(int effort , int[][] dist ,int row , int col, int newRow , int newCol){
         int n = heights.length;
         int m = heights[0].length;
         if(newRow < 0 || newRow >= n ||
